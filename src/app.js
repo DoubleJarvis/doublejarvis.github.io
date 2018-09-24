322e01a326cffa140f6fd75d05493b086ccea1c0
Nick <doublejarvis@gmail.com>
2018-09-24 17:44:00 +0300

    added parallax.js and scene

--- src/app.js
+++ src/app.js
@@ -1,3 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
   console.log("Hello from js")
+  console.log("Parallaxing...")
+  var scene = document.getElementById('scene');
+  var parallaxInstance = new Parallax(scene, {
+    relativeInput: true
+  });
+  parallaxInstance.friction(0.2, 0.2);
 })
